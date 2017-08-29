6969159620cedbc18b0efab1e7ff7c46c56fdc59
txgcwm <txgcwm@163.com>
2017-08-29 21:10:42 +0800

    use cmake to build big_int

--- base/big_int/build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-g++  -std=c++11  test.cpp arith_uint256.cpp  uint256.cpp  utilstrencodings.cpp  -I ./
--- base/big_int/test/test.cpp
+++ base/big_int/test/test.cpp
@@ -1,12 +1,15 @@
-#include "arith_uint256.h"
 #include <iostream>
 
+#include "big_int/arith_uint256.h"
 
 
-int main()
+
+int main(int argc, char** argv)
 {
   arith_uint256 t(std::string("1"));
   arith_uint256 b(std::string("1"));
   t += b;
   std::cout << "t + b: " << t.ToString() << std::endl;
+
+  return 0;
 }
