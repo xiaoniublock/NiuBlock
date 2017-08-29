3608753850113e7c8d9ba7f79a639ed146b67bd0
txgcwm <txgcwm@163.com>
2017-08-29 21:36:07 +0800

    add test

--- example/test/TestUniValue.cpp
+++ example/test/TestUniValue.cpp
@@ -12,6 +12,14 @@ int main(int argc, char** argv)
         printf("default is null!\n");
     }
 
+    if(uval.setBool(true)) {
+        printf("set bool success!\n");
+    } else {
+        printf("set bool fail!\n");
+    }
+
+    printf("get bool %s\n", uval.getBool() ? "true":"false");
+
     return 0;
 }
 
